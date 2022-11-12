aa63a3e11d8943682bf747fb66334868a377e0ae
Julian <julianoberhofer2@gmail.com>
2022-11-12 23:16:43 +0100

    V 4.0

--- src/app.js
+++ src/app.js
@@ -64,6 +64,8 @@ app2.use("/log",express.static(__dirname + "/log"));
 var socket_list={};
 var playerInQ = {};
 var playerIn = 0
+var playerInQ2 = {};
+var playerIn2 = 0
 
 var serverId=Math.random();
 
@@ -80,8 +82,12 @@ io.sockets.on("connection",function(socket){
     socket.turn="O";
     socket.point="0";
     socket.playss=0;
+    socket.jkl=0;
+    socket.durch=1;
     socket.type;
+    socket.tem;
     socket.aa=[false,false,false,false,false,false,false,false,false];
+    socket.mode;
     var fuck=false;
 
     socket.on("disconnect",function(){disc();delete socket_list[socket.id];});
@@ -110,30 +116,75 @@ io.sockets.on("connection",function(socket){
                 playerIn = 0;
                 consolelog("queue");
             }
-            else consolelog("Login")
+            else consolelog("Login");
         }
     }
 
-    socket.on("conn",function(valu){
+    socket.on("conn",function(valu,mode){
         socket.name = valu;
-        consolelog(socket.id+" has canged the name to "+socket.name);
+        socket.mode = mode;
+        consolelog(socket.id+" has canged the name to "+socket.name+" (Mode: "+mode+")");
         socket.queueM();
     });
 
     socket.on("turn",function(hh){
-        if((socket.aa[hh]===false)&&(socket.turn==socket.type))
-        {
-            socket.emit("turned",socket.type,socket.otherPlayer.type,hh);
-            socket.otherPlayer.emit("turned",socket.type,socket.otherPlayer.type,hh);
-            socket.aa[hh]="1";
-            socket.otherPlayer.aa[hh]="2";
-            socket.turn=socket.otherPlayer.type;
-            socket.otherPlayer.turn=socket.otherPlayer.type;
+        if(socket.turn==socket.type){
+            if(socket.mode == 1){
+                if(socket.aa[hh]===false)
+                {
+                    socket.emit("turned",socket.type,socket.otherPlayer.type,hh);
+                    socket.otherPlayer.emit("turned",socket.type,socket.otherPlayer.type,hh);
+                    socket.aa[hh]="1";
+                    socket.otherPlayer.aa[hh]="2";
+                    socket.turn=socket.otherPlayer.type;
+                    socket.otherPlayer.turn=socket.otherPlayer.type;
+                }
+            }
+            else if(socket.mode == 2){
+                if((socket.aa[hh]===false)&&(socket.jkl<6)){
+                    socket.emit("turned",socket.type,socket.otherPlayer.type,hh,"","move");
+                    socket.otherPlayer.emit("turned",socket.type,socket.otherPlayer.type,hh,"","move");
+                    socket.aa[hh]="1";
+                    socket.otherPlayer.aa[hh]="2";
+                    socket.jkl++;
+                    socket.otherPlayer.jkl++;
+                    socket.turn=socket.otherPlayer.type;
+                    socket.otherPlayer.turn=socket.otherPlayer.type;
+                }
+                else if(socket.jkl>=6){
+                    if((socket.durch=="1")&&(socket.aa[hh]=="1")){
+                        socket.emit("turned","","",hh,"","red");
+                        socket.otherPlayer.emit("turned","","",hh,"","red");
+                        socket.tem=hh;
+                        socket.durch="2";
+                    }
+                    else if((socket.durch=="2")&&(hh==socket.tem)){
+                        socket.emit("turned","","","",socket.tem,"rem");
+                        socket.otherPlayer.emit("turned","","","",socket.tem,"rem");
+                        socket.durch="1";
+                    }
+                    else if((socket.durch=="2")&&((socket.aa[hh]=="1"))){
+                        socket.emit("turned","","",hh,socket.tem,"chan");
+                        socket.otherPlayer.emit("turned","","",hh,socket.tem,"chan");
+                        socket.tem=hh;
+                    }
+                    else if((socket.durch=="2")&&(socket.aa[hh]===false)){
+                        socket.emit("turned",socket.type,socket.otherPlayer.type,hh,socket.tem,"fin");
+                        socket.otherPlayer.emit("turned",socket.type,socket.otherPlayer.type,hh,socket.tem,"fin");
+                        socket.aa[hh]="1";
+                        socket.otherPlayer.aa[hh]="2";
+                        socket.aa[socket.tem]=false;
+                        socket.otherPlayer.aa[socket.tem]=false;
+                        socket.turn=socket.otherPlayer.type;
+                        socket.otherPlayer.turn=socket.otherPlayer.type;
+                        socket.durch="1";
+                        socket.tem = undefined;
+                    }
+                }
+            }
+
             //Gewinn prüfung
-
-
             var awe="1";
-
             for(var fo=0;fo<9;fo=fo+3)
             {
                 if((socket.aa[fo]==socket.aa[fo+1])&&(socket.aa[fo+1]==socket.aa[fo+2])&&(socket.aa[fo+2]==awe))
@@ -162,13 +213,14 @@ io.sockets.on("connection",function(socket){
                 return;
             }
 
-            socket.playss++;
-            socket.otherPlayer.playss++;
-            if(socket.playss==9)
-            {
-                rese();
+            if(socket.mode==1){
+                socket.playss++;
+                socket.otherPlayer.playss++;
+                if(socket.playss==9)
+                {
+                    rese();
+                }
             }
-
         }
     });
 
@@ -204,42 +256,92 @@ io.sockets.on("connection",function(socket){
         socket.otherPlayer.aa=[3,3,3,3,3,3,3,3,3];
         socket.playss=0;
         socket.otherPlayer.playss=0;
+        socket.jkl=0;
+        socket.otherPlayer.jkl=0;
+        socket.durch=1;
+        socket.otherPlayer.durch=1;
     }
     socket.queueM = function(){
-        playerInQ[playerIn]=socket.id;
-        playerIn++;
-        //queue manager
-        if((playerIn<2)&&(socket.online==false)&&(socket.queu==false)) {
-            socket.emit("que",1);
-            socket.online=false;
-            socket.queu=true;
+        if(socket.mode == 1){
+            playerInQ[playerIn]=socket.id;
+            playerIn++;
+            //queue manager
+            if((playerIn<2)&&(socket.online==false)&&(socket.queu==false)) {
+                socket.emit("que",1);
+                socket.online=false;
+                socket.queu=true;
+            }
+            else if((playerIn==2)&&(socket.online==false)&&(socket.queu==false)) {
+                socket.otherPlayer=socket_list[playerInQ[0]];
+                socket.type="X";
+                socket.otherPlayer.type="O";
+                socket.otherPlayer.otherPlayer=socket_list[playerInQ[1]];//or = socket but im fancy =)
+                socket.emit("que",2,socket.type,socket.otherPlayer.type,socket.otherPlayer.name);
+                socket.otherPlayer.emit("que",2,socket.otherPlayer.type,socket.type,socket.name);
+                socket.online=true;
+                socket.otherPlayer.online=true;
+                socket.queu=false;
+                socket.otherPlayer.queu=false;
+                socket.playss=0;
+                socket.otherPlayer.playss=0;
+                socket.jkl=0;
+                socket.otherPlayer.jkl=0;
+                socket.durch=1;
+                socket.otherPlayer.durch=1;
+                socket.turn="O";
+                socket.otherPlayer.turn="O";
+                socket.otherPlayer.point="0";
+                socket.aa=[false,false,false,false,false,false,false,false,false];
+                socket.otherPlayer.aa=[false,false,false,false,false,false,false,false,false];
+                socket.point="0";
+                consolelog("Match betwen:"+socket_list[playerInQ[0]].name+" and "+socket_list[playerInQ[1]].name);
+                playerInQ = {};
+                playerIn = 0;
+                socket.emit("startChat");
+                socket.otherPlayer.emit("startChat");
+            }
+            else socket.emit("erro");
         }
-        else if((playerIn==2)&&(socket.online==false)&&(socket.queu==false)) {
-            socket.otherPlayer=socket_list[playerInQ[0]];
-            socket.otherPlayer.otherPlayer=socket_list[playerInQ[1]];//or = socket but im fancy =)
-            socket.type="X";
-            socket.otherPlayer.type="O";
-            socket.emit("que",2,socket.type,socket.otherPlayer.type,socket.otherPlayer.name);
-            socket.otherPlayer.emit("que",2,socket.otherPlayer.type,socket.type,socket.name);
-            socket.online=true;
-            socket.otherPlayer.online=true;
-            socket.queu=false;
-            socket.otherPlayer.queu=false;
-            socket.playss=0;
-            socket.otherPlayer.playss=0;
-            socket.turn="O";
-            socket.otherPlayer.turn="O";
-            socket.otherPlayer.point="0";
-            socket.aa=[false,false,false,false,false,false,false,false,false];
-            socket.otherPlayer.aa=[false,false,false,false,false,false,false,false,false];
-            socket.point="0";
-            socket.emit("startChat");
-            socket.otherPlayer.emit("startChat");
-            consolelog("Match betwen:"+socket_list[playerInQ[0]].name+" and "+socket_list[playerInQ[1]].name);
-            playerInQ = {};
-            playerIn = 0;
+        else if(socket.mode == 2){
+            playerInQ2[playerIn2]=socket.id;
+            playerIn2++;
+            //queue manager
+            if((playerIn2<2)&&(socket.online==false)&&(socket.queu==false)) {
+                socket.emit("que",1);
+                socket.online=false;
+                socket.queu=true;
+            }
+            else if((playerIn2==2)&&(socket.online==false)&&(socket.queu==false)) {
+                socket.otherPlayer=socket_list[playerInQ2[0]];
+                socket.otherPlayer.otherPlayer=socket_list[playerInQ2[1]];//or = socket but im fancy =)
+                socket.type="X";
+                socket.otherPlayer.type="O";
+                socket.emit("que",2,socket.type,socket.otherPlayer.type,socket.otherPlayer.name);
+                socket.otherPlayer.emit("que",2,socket.otherPlayer.type,socket.type,socket.name);
+                socket.online=true;
+                socket.otherPlayer.online=true;
+                socket.queu=false;
+                socket.otherPlayer.queu=false;
+                socket.playss=0;
+                socket.otherPlayer.playss=0;
+                socket.jkl=0;
+                socket.otherPlayer.jkl=0;
+                socket.durch=1;
+                socket.otherPlayer.durch=1;
+                socket.turn="O";
+                socket.otherPlayer.turn="O";
+                socket.otherPlayer.point="0";
+                socket.aa=[false,false,false,false,false,false,false,false,false];
+                socket.otherPlayer.aa=[false,false,false,false,false,false,false,false,false];
+                socket.point="0";
+                consolelog("Match betwen:"+socket_list[playerInQ2[0]].name+" and "+socket_list[playerInQ2[1]].name);
+                playerInQ2 = {};
+                playerIn2 = 0;
+                socket.emit("startChat");
+                socket.otherPlayer.emit("startChat");
+            }
+            else socket.emit("erro");
         }
-        else socket.emit("erro");
     }
 
     socket.on("sendMessage",function(ff){
@@ -258,17 +360,17 @@ io.sockets.on("connection",function(socket){
 
 
 
-    socket.on("man",function(){for(var i in socket_list)consolelog("llll");})
+
+    socket.on("man",function(){for(var i in socket_list)consolelog("llll");});
 
     socket.emit("serverRestart",serverId);
 });
 
-consolelog("sockel initialiesiert","green");
+consolelog("sockel initialiesiert");
 
-function consolelog(gg,hh){
+function consolelog(gg){
     let aa=new Date;
-    if(hh) console.log("["+aa.getDate()+"."+(aa.getMonth()+1)+"-"+aa.getHours()+":"+aa.getMinutes()+"] <span style='color:"+hh+"'>"+gg+"</span>");
-    else console.log("["+aa.getDate()+"."+(aa.getMonth()+1)+"-"+aa.getHours()+":"+aa.getMinutes()+"] "+gg);
+    console.log("["+aa.getDate()+"."+(aa.getMonth()+1)+"-"+aa.getHours()+":"+aa.getMinutes()+"] "+gg);
 }
 
-consolelog("Start finished","green");
+consolelog("Start finished");
